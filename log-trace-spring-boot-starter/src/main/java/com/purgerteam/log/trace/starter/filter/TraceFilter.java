c0aada8b612e4631d8facaad6e4bf8ad2732d1ce
Purgeyao <zhaoy@deepblueai.com>
2019-11-19 17:33:22 +0800

    Refactor TraceFilter by adding TraceContentFactory#storageMDC method

--- log-trace-spring-boot-starter/src/main/java/com/purgerteam/log/trace/starter/filter/TraceFilter.java
+++ log-trace-spring-boot-starter/src/main/java/com/purgerteam/log/trace/starter/filter/TraceFilter.java
@@ -1,14 +1,16 @@
 package com.purgerteam.log.trace.starter.filter;
 
 import com.purgerteam.log.trace.starter.Constants;
+import com.purgerteam.log.trace.starter.instrument.TraceContentFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-import org.springframework.util.StringUtils;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author purgeyao
@@ -21,23 +23,16 @@ public class TraceFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = ((HttpServletRequest) servletRequest);
+
         // "traceId"
         String headerTraceId = request.getHeader(Constants.LEGACY_TRACE_ID_NAME);
-        log.debug("TraceFilter headerTraceId {}", headerTraceId);
-        // 如果为空，则表示第一次访问，即上游服务端的请求
-        if (StringUtils.isEmpty(headerTraceId)) {
-            MDC.put(Constants.LEGACY_TRACE_ID_NAME, TraceIdUtil.traceIdString());
-        } else {
-            MDC.put(Constants.LEGACY_TRACE_ID_NAME, headerTraceId);
-        }
-
         // "ParentName"
         String headerParentName = request.getHeader(Constants.LEGACY_PARENT_SERVICE_NAME);
-        if (StringUtils.isEmpty(headerTraceId)) {
-            MDC.put(Constants.LEGACY_PARENT_SERVICE_NAME, "this");
-        } else {
-            MDC.put(Constants.LEGACY_PARENT_SERVICE_NAME, headerParentName);
-        }
+
+        Map<String, String> map = new HashMap<>(16);
+        map.put(Constants.LEGACY_TRACE_ID_NAME, headerTraceId);
+        map.put(Constants.LEGACY_PARENT_SERVICE_NAME, headerParentName);
+        TraceContentFactory.storageMDC(map);
 
         filterChain.doFilter(servletRequest, servletResponse);
         MDC.clear();
--- log-trace-spring-boot-starter/src/main/java/com/purgerteam/log/trace/starter/instrument/TraceContentFactory.java
+++ log-trace-spring-boot-starter/src/main/java/com/purgerteam/log/trace/starter/instrument/TraceContentFactory.java
@@ -1,10 +1,15 @@
 package com.purgerteam.log.trace.starter.instrument;
 
 import com.purgerteam.log.trace.starter.Constants;
+import com.purgerteam.log.trace.starter.filter.TraceIdUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -13,17 +18,54 @@ import java.util.Map;
  */
 public class TraceContentFactory implements EnvironmentAware {
 
-    private Environment environment;
+    private static final Logger log = LoggerFactory.getLogger(TraceContentFactory.class);
+
+    private static Environment environment;
+
+    /**
+     * 储存本地MDC
+     *
+     * @param traceContentMap 内容集合
+     */
+    public static void storageMDC(Map<String, String> traceContentMap) {
+        String headerTraceId = traceContentMap.get(Constants.LEGACY_TRACE_ID_NAME);
+        log.debug("Trace traceId {}", headerTraceId);
+        // 如果为空，则表示第一次访问，即上游服务端的请求
+        if (StringUtils.isEmpty(headerTraceId)) {
+            MDC.put(Constants.LEGACY_TRACE_ID_NAME, TraceIdUtil.traceIdString());
+        } else {
+            MDC.put(Constants.LEGACY_TRACE_ID_NAME, headerTraceId);
+        }
+
+        // "ParentName"
+        String headerParentName = traceContentMap.get(Constants.LEGACY_PARENT_SERVICE_NAME);
+        if (StringUtils.isEmpty(headerTraceId)) {
+            MDC.put(Constants.LEGACY_PARENT_SERVICE_NAME, "this");
+        } else {
+            MDC.put(Constants.LEGACY_PARENT_SERVICE_NAME, headerParentName);
+        }
+    }
 
     public Map<String, String> assemblyTraceContent() {
-        Map<String, String> copyOfContextMap = MDC.getCopyOfContextMap();
+        return buildTraceContent();
+    }
+
+    public static Map<String, String> assemblyTraceContentStatic() {
+        return buildTraceContent();
+    }
+
+    private static Map<String, String> buildTraceContent() {
+        Map<String, String> traceContentMap = MDC.getCopyOfContextMap();
+        if (traceContentMap == null) {
+            traceContentMap = new HashMap<>(16);
+        }
         String serviceName = environment.getProperty("spring.application.name");
-        copyOfContextMap.put(Constants.LEGACY_PARENT_SERVICE_NAME, serviceName);
-        return copyOfContextMap;
+        traceContentMap.put(Constants.LEGACY_PARENT_SERVICE_NAME, serviceName);
+        return traceContentMap;
     }
 
     @Override
     public void setEnvironment(Environment environment) {
-        this.environment = environment;
+        TraceContentFactory.environment = environment;
     }
 }
